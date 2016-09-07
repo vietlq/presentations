845f6f330c986ba1976163de2d0a3208bf0edba7
Viet Le <vietlq85@gmail.com>
2016-09-07 02:07:00 +0100

    Added back Livechat

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ client:
 	watchify src/main.jsx -v -t [ babelify --presets [ es2015 react ] ] -o public/js/main.js
 
 build:
-	NODE_ENV=production webpack --progress -p && babel --minified src/server -o server.js
+	NODE_ENV=production webpack --progress -p
 
 run: server.js
 	nodemon --verbose server.js
--- src/components/PageBody.jsx
+++ src/components/PageBody.jsx
@@ -6,14 +6,16 @@ var PageBody = React.createClass({
             <div className="page-body-container">
                 <div className="col-sm-12">
                     <div className="well">
-                        <div className="row news-item-main">
-                            <div className="col-sm-6">
+                        <div className="row">
+                            <div className="col-sm-7">
                             </div>
-                            <div className="col-sm-6">
-                                <iframe allowFullScreen frameBorder="0" height="270" width="480"
+                            <div className="col-sm-5">
+                                <iframe className="live-stream" allowFullScreen frameBorder="0"
+                                    height="234" width="416"
                                     src="https://www.youtube.com/embed/-GnebGxHee4"></iframe>
                                 <br/>
-                                <iframe allowFullScreen frameBorder="0" height="270" width="480"
+                                <iframe allowFullScreen frameBorder="0"
+                                    height="234" width="416"
                                     src="https://www.youtube.com/live_chat?v=gtj6Yc4MWxU&embed_domain=vietlq.github.io"></iframe>
                                 <div id="disqus_thread"></div>
                             </div>
