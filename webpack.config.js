e30da0c8459b28c1ef3a5debc7b6df04322c4443
Viet Le <vietlq85@gmail.com>
2016-09-07 07:07:02 +0100

    Removed dev debug

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,15 @@ var path = require('path');
 var BUILD_DIR = path.resolve(__dirname, 'public/js');
 var APP_DIR = path.resolve(__dirname, 'src');
 
+var ForceProdEnv = new webpack.DefinePlugin({
+    "process.env": {
+        NODE_ENV: JSON.stringify("production")
+    }
+});
+
 // Add this handful of plugins that optimize the build when we're in production
 var plugins = (process.env.NODE_ENV === 'production') ? [
+    ForceProdEnv,
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin()
